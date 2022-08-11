9247c50026ae9dcb743bc4b351fc9cfa01959b26
dinhvietahn23 <dinhvietahn23@gmail.com>
2022-08-11 17:00:29 +0700

    sign innnnnnn

---
+++ vnvoice-mobile/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "vnvoicea4cb866e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- vnvoice-mobile/lib/main.dart
+++ vnvoice-mobile/lib/main.dart
@@ -1,12 +1,54 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
 import "package:flutter/material.dart";
 import 'package:vnvoicemobile/screen/SignIn.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+
+import 'amplifyconfiguration.dart';
+
+
 
 void main () {
-  runApp(Main());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ProviderScope(child:Main()));
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
   const Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  bool _amplifyConfigured = false;
+  AmplifyAuthCognito authCognito = AmplifyAuthCognito();
+
+  Future<void> _configAmplify() async {
+    if(!_amplifyConfigured) {
+
+    }
+    try{
+      await Amplify.addPlugins([
+        authCognito
+      ]);
+      await Amplify.configure(amplifyconfig);
+      print("configure");
+      setState((){
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print("error: \n $e");
+
+    }
+
+  }
+  @override
+  void initState() {
+    super.initState();
+    _configAmplify();
+  }
+
 
   @override
   Widget build(BuildContext context) {
