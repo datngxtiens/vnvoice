0f14578897e09def611505fcc08cfc882b572fe3
Nguyen Tien Dat <datngxtiens@gmail.com>
2022-08-24 22:57:30 +0700

    Update lambda function

--- vnvoice-api/vnvoice-get-post-detail/lambda_function.py
+++ vnvoice-api/vnvoice-get-post-detail/lambda_function.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from vnvoice.db.postgres.connector import PostgresConnector
@@ -8,6 +9,11 @@ from vnvoice.util import get_gateway_response, get_logger
 logger = get_logger(__name__)
 postgres = PostgresConnector()
 
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
+
 def lambda_handler(event, context):
     try:
         post_id = event["queryStringParameters"]["id"]
@@ -16,7 +22,7 @@ def lambda_handler(event, context):
 
         comment_query = (f"SELECT {pgt.COMMENT.value}.author_id, {pgt.COMMENT.value}.id, "
                          f"{pgt.ACCOUNT.value}.username, {pgt.COMMENT.value}.text, "
-                         f"{pgt.ACCOUNT.value}.img_url "
+                         f"post_comment.created_at, {pgt.ACCOUNT.value}.img_url "
                          f"FROM {pgt.COMMENT.value} JOIN {pgt.ACCOUNT.value} "
                          f"ON {pgt.ACCOUNT.value}.id = {pgt.COMMENT.value}.author_id "
                          f"WHERE {pgt.COMMENT.value}.post_id = '{post_id}' "
@@ -24,10 +30,11 @@ def lambda_handler(event, context):
 
         postgres.execute(comment_query)
         for comment in postgres.cursor.fetchall():
-            (author_id, comment_id, username, text, author_img_url) = comment
+            (author_id, comment_id, username, text, created_at, author_img_url) = comment
 
             comment = {
                 "author_id": author_id,
+                "created_at": created_at,
                 "comment_id": comment_id,
                 "author": username,
                 "author_img_url": author_img_url,
@@ -35,7 +42,7 @@ def lambda_handler(event, context):
             }
 
             comment_query = (f"SELECT {pgt.COMMENT.value}.id, {pgt.ACCOUNT.value}.username, "
-                             f"{pgt.ACCOUNT.value}.id as user_id,"
+                             f"{pgt.ACCOUNT.value}.id as user_id, post_comment.created_at, "
                              f"{pgt.COMMENT.value}.text, {pgt.ACCOUNT.value}.img_url "
                              f"FROM {pgt.COMMENT.value} "
                              f"JOIN {pgt.ACCOUNT.value} "
@@ -45,10 +52,11 @@ def lambda_handler(event, context):
 
             child_comments = []
             for child in postgres.cursor.fetchall():
-                (cid, cauthor, aid, txt, aurl) = child
+                (cid, cauthor, aid, created, txt, aurl) = child
                 ccomment = {
                     "author_id": aid,
                     "comment_id": cid,
+                    "created_at": created,
                     "author": cauthor,
                     "author_img_url": aurl,
                     "text": txt
@@ -63,7 +71,7 @@ def lambda_handler(event, context):
             "data": comments
         }
 
-        return get_gateway_response(StatusCode.SUCCESS.value, json.dumps(response))
+        return get_gateway_response(StatusCode.SUCCESS.value, json.dumps(response, default=datetime_handler))
 
     except Exception as err:
         logger.error(f"Exception: {str(err)}")
